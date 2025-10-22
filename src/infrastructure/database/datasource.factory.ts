5f312410052bbd2dfd463134ce74cbdb51afee75
Pablo Kaza <kazalukianpablo@gmail.com>
2025-10-22 12:13:38 -0300

    focker

---
+++ src/infrastructure/database/datasource.factory.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+// import { PostgresDataSource } from './postgres/postgres.datasource';
+// import { MysqlDataSource } from './mysql/mysql.datasource';
+// import { UserEntity } from '../../domain/user.entity';
+
+// export const createDataSource = (appSource: string): DataSource => {
+//   switch (appSource) {
+//     case 'postgres':
+//       return PostgresDataSource;
+//     case 'mysql':
+//       return MysqlDataSource;
+//     default:
+//       throw new Error(`Unknown datasource: ${appSource}`);
+//   }
+// };
---
+++ src/infrastructure/database/postgres/postgres.datasource.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+// import { UserEntity } from '../../../domain/user.entity';
+
+export const PostgresDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  //   entities: [UserEntity],
+  synchronize: true,
+});
