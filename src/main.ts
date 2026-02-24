825643f0fa03bd1ea4d686f74676ec191d2bd55a
Pablo Kaza <kazalukianpablo@gmail.com>
2026-02-24 18:44:19 -0300

    fix(package.json) Change script start && start:prod

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { GraphQLExceptionFilter } from './common/filters/gpl-exeption.filter';
 
 async function bootstrap() {
