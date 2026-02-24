f9bdf8061cac21af42d3b3f03c7bf1d134b50dff
Pablo Kaza <kazalukianpablo@gmail.com>
2026-02-24 18:46:41 -0300

    fix(main.ts) Change port for .env

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { GraphQLExceptionFilter } from './common/filters/gpl-exeption.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
   app.useGlobalFilters(new GraphQLExceptionFilter());
-  // app.useGlobalPipes(
-  //   new ValidationPipe({ whitelist: true, forbidNonWhitelisted: false }),
-  // );
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
