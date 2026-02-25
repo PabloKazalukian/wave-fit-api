b38059d48fd7bb4847b4ba93e4261f3e850313b4
Pablo Kaza <kazalukianpablo@gmail.com>
2026-02-25 15:08:34 -0300

    fix(callback) Google changes, whether for production or development

--- src/modules/auth/google/google.strategy.ts
+++ src/modules/auth/google/google.strategy.ts
@@ -9,7 +9,10 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      callbackURL: 'http://localhost:3000/auth/google-redirect',
+      callbackURL:
+        process.env.NODE_ENV === 'development'
+          ? 'http://localhost:3000/auth/google-redirect'
+          : 'https://wave-fit.vercel.app/auth/google-redirect',
       scope: ['email', 'profile'],
     });
   }
